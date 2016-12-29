e32ab1f93bdc9f6b821b936bb566e083b6c40ec2
Ramalingam Thangavel <ram@miamidade.gov>
2016-12-29 16:41:31 -0500

    first commit

---
+++ src/main/java/com/example/ReleasetestApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReleasetestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReleasetestApplication.class, args);
+	}
+}
